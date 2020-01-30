4b2e18222a89c02a862a3d972da130a3ad4a6bbc
MichaelKurzak <55596437+MichaelKurzak@users.noreply.github.com>
2020-01-30 18:33:12 +0100

    giving dimensions to the playing field

--- src/main/java/snake/RenderPanel.java
+++ src/main/java/snake/RenderPanel.java
@@ -12,13 +12,19 @@ public class RenderPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(new Color(curColor));
-        g.fillRect(0,0,600,600);
+        g.fillRect(0, 0, 600, 600);
         curColor++;
         Snake snake = Snake.snake;
-        g.setColor(Color.RED);
-        for(Point point : snake.snakeParts){
-            g.fillRect(point.x * Snake.SCALE,point.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+        g.setColor(Color.BLUE);
+        for (Point point : snake.snakeParts) {
+            g.fillRect(point.x * Snake.SCALE, point.y * Snake.SCALE,
+                    Snake.SCALE, Snake.SCALE);
         }
-        g.fillRect(snake.head.x * Snake.SCALE, snake.head.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+        g.fillRect(snake.head.x * Snake.SCALE, snake.head.y * Snake.SCALE,
+                Snake.SCALE, Snake.SCALE);
+        g.setColor(Color.RED);
+        g.fillRect(snake.cherry.x * Snake.SCALE, snake.cherry.y * Snake.SCALE,
+                Snake.SCALE, Snake.SCALE);
+
     }
 }
--- src/main/java/snake/Snake.java
+++ src/main/java/snake/Snake.java
@@ -4,10 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Snake implements ActionListener {
+public class Snake implements ActionListener, KeyListener {
 
     public JFrame jframe;
     public RenderPanel renderPanel;
@@ -15,24 +17,40 @@ public class Snake implements ActionListener {
     public static Snake snake;
     public ArrayList<Point> snakeParts = new ArrayList<Point>();
     public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
-    public int ticks = 0, direction = DOWN, score, tailLength;
+    public int ticks = 0, direction = DOWN, score, tailLength = 10;
     public Point head, cherry;
     public Random random;
-    public boolean over = false;
+    public boolean over = false, paused;
     public Dimension dim;
 
     public Snake() {
         dim = Toolkit.getDefaultToolkit().getScreenSize();
         jframe = new JFrame("Snake");
         jframe.setVisible(true);
-        jframe.setSize(600, 600);
-        jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2,
+        jframe.setSize(617, 639);
+        jframe.setResizable(false);
+        jframe.setLocation(dim.width / 2- jframe.getWidth() / 2,
                 dim.height / 2 - jframe.getHeight() / 2);
         jframe.add(renderPanel = new RenderPanel());
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        head = new Point(0, 0);
+        jframe.addKeyListener(this);
+        startGame();
+    }
+
+    public void startGame() {
+        over = false;
+        paused = false;
+        score = 0;
+        tailLength = 1;
+        direction = DOWN;
+        head = new Point(0, -1);
         random = new Random();
-        cherry = new Point(dim.width / SCALE, dim.height / SCALE);
+        snakeParts.clear();
+        cherry = new Point(random.nextInt(60), random.nextInt(60));
+        System.out.println(cherry.x + ", " + cherry.y);
+        for (int i = 0; i < tailLength; i++) {
+            snakeParts.add(new Point(head.x, head.y));
+        }
         timer.start();
     }
 
@@ -41,42 +59,74 @@ public class Snake implements ActionListener {
         renderPanel.repaint();
         ticks++;
 
-        if (ticks % 10 == 0 && head != null && !over) {
+        if (ticks % 5 == 0 && head != null && !over && !paused) {
             snakeParts.add(new Point(head.x, head.y));
             if (direction == UP)
-                if (head.y - 1 > 0)
+                if (head.y - 1 >= 0)
                     head = new Point(head.x, head.y - 1);
                 else
-            over = true;
+                    over = true;
             if (direction == DOWN)
-                if (head.y + 1 < dim.height / SCALE)
+                if (head.y + 1 < 60)
                     head = new Point(head.x, head.y + 1);
                 else
-            over = true;
+                    over = true;
             if (direction == LEFT)
-                if (head.x - 1 > 0)
+                if (head.x - 1 >= 0)
                     head = new Point(head.x - 1, head.y);
                 else
-            over = true;
+                    over = true;
             if (direction == RIGHT)
-                if (head.x + 1 < dim.width / SCALE)
+                if (head.x + 1 < 60)
                     head = new Point(head.x + 1, head.y);
                 else
-            over = true;
+                    over = true;
+            if (snakeParts.size() > tailLength)
+                snakeParts.remove(0);
             if (cherry != null) {
                 if (head.equals(cherry)) {
                     score += 10;
                     tailLength++;
-                    cherry.setLocation(dim.width / SCALE, dim.height / SCALE);
-
+                    cherry.setLocation(random.nextInt(60), random.nextInt(60));
                 }
             }
         }
+
     }
 
     public static void main(String[] args) {
         snake = new Snake();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int i = e.getKeyCode();
+        if (i == KeyEvent.VK_A && direction != RIGHT)
+            direction = LEFT;
+        if (i == KeyEvent.VK_D && direction != LEFT)
+            direction = RIGHT;
+        if (i == KeyEvent.VK_W && direction != DOWN)
+            direction = UP;
+        if (i == KeyEvent.VK_S && direction != UP)
+            direction = DOWN;
+        if (i == KeyEvent.VK_SPACE)
+            if (over)
+                startGame();
+            else
+                paused = !paused;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
+
+
+
 
